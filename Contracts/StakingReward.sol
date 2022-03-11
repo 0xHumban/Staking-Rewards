508b661f913dbb6570b1d787e792a5bfb994053b
Humbert Evan <evan.humbert@outlook.com>
2022-03-11 04:31:26 +0100

    Update StakingReward.sol

--- Contracts/StakingReward.sol
+++ Contracts/StakingReward.sol
@@ -8,8 +8,9 @@ contract StakingReward {
     uint public rewardRate = 100;
     uint public lastUpdateTime;
     uint public rewardPerTokenStored;
+    uint le18 = 1000000000000000000;
 
-    mapping(address => uint) public userRewardperTokenPaid;
+    mapping(address => uint) public userRewardPerTokenPaid;
     mapping(address => uint) public rewards;
 
     uint private _totalSupply;
@@ -21,20 +22,38 @@ contract StakingReward {
     }
 
     function rewardPerToken() public view returns (uint) {
-
+        if (_totalSupply == 0) {
+            return 0;
+        }
+        return rewardPerTokenStored + (
+            rewardRate * (block.timestamp - lastUpdateTime) * le18 / _totalSupply
+            );
     }
 
     function earned(address account) public view returns (uint){
-
+        return (
+            _balances[account] * rewardPerToken() - userRewardPerTokenPaid[account]) / le18;
     }
 
     modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = block.timestamp;
 
+        rewards[account] = earned(account);
+        userRewardPerTokenPaid[account] = rewardPerTokenStored;
         _;
     }
 
     function stake(uint _amount) external updateReward(msg.sender) {
+        _totalSupply += _amount;
+        _balances[msg.sender] += _amount;
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+    }
 
+    function withdraw(uint _amount) external updateReward(msg.sender) {
+        _totalSupply -= _amount;
+        _balances[msg.sender] -= _amount;
+        stakingToken.transfer(msg.sender, _amount);
     }
 
     function getReward() external updateReward(msg.sender) {
